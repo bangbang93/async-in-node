148c614e592b89b3eae2bddd265f0a83cd6c84e1
bangbang93 <bangbang93@163.com>
2016-11-22 18:13:39 +0800

    Bluebird

--- code/08.Promise.js
+++ code/08.Promise.js
@@ -3,3 +3,21 @@
  */
 'use strict';
 const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+
+
+fs.readFileAsync('01.EventEmitter.js')
+  .then((data)=>{
+    return fs.readFileAsync('02.Callback.js');
+  })
+  .then((data)=>{
+    console.log(data);
+  });
+
+Promise.all([
+  fs.readFileAsync('01.EventEmitter.js'),
+  fs.readFileAsync('02.Callback.js'),
+  fs.readFileAsync('03.Callback2.js'),
+]).then((res)=>{
+  console.log(Buffer.concat(res).toString());
+});
